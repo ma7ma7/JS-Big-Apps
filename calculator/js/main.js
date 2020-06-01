4d4f26a0bc7c5e459869f26479f19ce659c5ff2d
mahmoud <mahmoudamane5@gmail.com>
2020-06-01 21:45:16 +0100

    AC fonctionalitie

--- calculator/js/main.js
+++ calculator/js/main.js
@@ -3,3 +3,30 @@
  * ? author : Amane Mahmoud
  * ? version : 1.0
  */
+
+//  Start the reset button
+const reset = document.getElementsByClassName('clear');
+const result = document.getElementsByClassName('result');
+
+reset[0].addEventListener('click', ()=> {
+    result[0].children[1].textContent = '0';
+});
+
+// Operations
+
+const operation = (operationType) => {
+    return function(a, b) {
+        switch (operationType) {
+            case "addition":
+                return a + b;
+            case "substraction":
+                return a - b;
+        }
+    }
+}
+
+const add = operation("addition");
+console.log(add(11, 15));
+
+const sub = operation("substraction");
+console.log(sub(11, 15));
